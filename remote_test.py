0c136b4558b9dbaa43e8c50a51c590d89eb081e0
Lucky Zhang <lucky.univ@gmail.com>
2014-03-14 20:33:00 -0400

    added new sequential local test data

--- remote_test.py
+++ remote_test.py
@@ -9,7 +9,7 @@ import client
 import sys
 import time
 
-c = client.Client("brahorn")
+c = client.Client()
 lookup_results = {}
 search_results = {}
 buy_results = {}
@@ -32,10 +32,10 @@ def main():
 
       if test_type == 'thread' and len(sys.argv) >=3:
         thread_num = int(sys.argv[3])
-        f = open("log/%s_multiple_test_on_%s_with_%d_threads\n" % (test_type, method_name, thread_num), 'w')
+        f = open("log/%s_multiple_test2_on_%s_with_%d_threads\n" % (test_type, method_name, thread_num), 'w')
         repeat_thread_test(thread_num, method_name, method)
       elif test_type == 'seq':
-        f = open("log/%s_multiple_test_on_%s" % (test_type, method_name), 'w')
+        f = open("log/%s_test2_on_%s" % (test_type, method_name), 'w')
         repeat_seq_test(method_name, method)
     f.write("\n\n")
 
